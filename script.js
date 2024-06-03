fbb9e14c40d9524025f4505a6cbfd1fb25028299
HolyMikeB <bnkrdicy@aol.com>
2024-06-03 17:19:39 -0500

    Fix bugs with game func logging score and computer selection

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function computerPlay() {
     var response = responsesArray[responseRandom];
     return response
 }
-let playerSelection;
-let computerSelection;
+const playerSelection = '';
+const computerSelection = '';
 let computerScore = 0;
 let playerScore = 0;
 
@@ -17,8 +17,6 @@ let playerScore = 0;
 function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay().toLowerCase();
     playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-    computerScore = 0;
-    playerScore = 0;
 
     console.log(computerSelection);
     /*take's player input and compares it to the computer's selection to determine the winner of the round and adds 1 to the winner's score*/
@@ -39,19 +37,22 @@ function playRound(playerSelection, computerSelection) {
         playerScore = ++playerScore
         return 'You won! ' + capitalize(playerSelection) + ' beats ' + computerSelection + '. The score is ' + playerScore + ' to ' + computerScore + '.';
     }
+
 }
     /*game function*/
 function game() {
     for (let i = 0; i < 5; i++) {
-        playRound()
-        if (computerScore > playerScore) {
-            return 'Sorry! you lost ' + computerScore + ' to ' + playerScore + '!';
-        } else if (playerScore > computerScore) {
-            return 'Congratulations! You won ' + playerScore + ' to ' + computerScore + '!';
-        } else {
-            return 'It\'s a tie!';
-        }
+        console.log(playRound())
     }
+
+    if (computerScore > playerScore) {
+        return 'Sorry! you lost ' + computerScore + ' to ' + playerScore + '!';
+    } else if (playerScore > computerScore) {
+        return 'Congratulations! You won ' + playerScore + ' to ' + computerScore + '!';
+    } else {
+        return 'It\'s a tie!';
+    }
+
 }
 /*function for capitalizing the first letter in a string*/
 function capitalize(string) {
